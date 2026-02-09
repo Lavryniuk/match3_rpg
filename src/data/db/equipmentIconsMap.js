63449eb263896ede06e1c215f82bd037cb75b792
Artur <exalt.webdev.97@gmail.com>
2026-02-09 20:15:59 +0100

    feat(assets): add equipment icons registry

---
+++ src/data/db/equipmentIconsMap.js
@@ -0,0 +1,39 @@
+import paladinHelmet from "../../assets/img/slots/helmet/paladin_helmet.png";
+import paladinBody from "../../assets/img/slots/body/paladin_body.png";
+import paladinWeapon from "../../assets/img/slots/weapon/paladin_weapon.png";
+import paladinOffHand from "../../assets/img/slots/off_hand/paladin_off_hand.png";
+import paladinBoots from "../../assets/img/slots/boots/paladin_boots.png";
+import sorceressHelmet from "../../assets/img/slots/helmet/sorceress_helmet.png";
+import sorceressBody from "../../assets/img/slots/body/sorceress_body.png";
+import sorceressWeapon from "../../assets/img/slots/weapon/sorceress_weapon.png";
+import sorceressOffHand from "../../assets/img/slots/off_hand/sorceress_off_hand.png";
+import sorceressBoots from "../../assets/img/slots/boots/sorceress_boots.png";
+import archerHelmet from "../../assets/img/slots/helmet/archer_helmet.png";
+import archerBody from "../../assets/img/slots/body/archer_body.png";
+import archerWeapon from "../../assets/img/slots/weapon/archer_weapon.png";
+import archerOffHand from "../../assets/img/slots/off_hand/archer_off_hand.png";
+import archerBoots from "../../assets/img/slots/boots/archer_boots.png";
+
+export const EQUIPMENT_ICONS = {
+  paladin: {
+    helmet: paladinHelmet,
+    body: paladinBody,
+    weapon: paladinWeapon,
+    offHand: paladinOffHand,
+    boots: paladinBoots,
+  },
+  sorceress: {
+    helmet: sorceressHelmet,
+    body: sorceressBody,
+    weapon: sorceressWeapon,
+    offHand: sorceressOffHand,
+    boots: sorceressBoots,
+  },
+  archer: {
+    helmet: archerHelmet,
+    body: archerBody,
+    weapon: archerWeapon,
+    offHand: archerOffHand,
+    boots: archerBoots,
+  },
+};
