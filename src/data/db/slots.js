fe7f378b6b2d36ee25715936038a2db7c88931ad
Artur <exalt.webdev.97@gmail.com>
2026-02-14 17:18:34 +0100

    chore: rename weapon shadow asset for consistency

--- src/data/db/slots.js
+++ src/data/db/slots.js
@@ -1,6 +1,6 @@
 import helmetShadow from "../../assets/icons/shadow-slots/helmet-shadow.png";
 import bodyShadow from "../../assets/icons/shadow-slots/body-shadow.png";
-import weaponShadow from "../../assets/icons/shadow-slots/weapon-shadow-1.png";
+import weaponShadow from "../../assets/icons/shadow-slots/weapon-shadow.png";
 import offHandShadow from "../../assets/icons/shadow-slots/shield-shadow.png";
 import bootsShadow from "../../assets/icons/shadow-slots/boots-shadow.png";
 
