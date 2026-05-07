10fbd1a0d067122cef530c6c3126b20c4bd17701
Artur <exalt.webdev.97@gmail.com>
2026-05-07 16:59:45 +0200

    feat: add equip and unequip item logic for character inventory

--- src/game/GameProvider.jsx
+++ src/game/GameProvider.jsx
@@ -1,6 +1,8 @@
 import { createContext, useContext, useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 
+import { characters } from "../classes/charactersConfig";
+
 const GameContext = createContext();
 
 export default function GameProvider({ children }) {
@@ -22,6 +24,32 @@ export default function GameProvider({ children }) {
     localState?.equipmentsInventory ?? []
   );
 
+  const [charactersInventory, setCharactersInventory] = useState(
+    localState?.charactersInventory ?? {
+      sorceress: {
+        helmet: null,
+        body: null,
+        weapon: null,
+        offHand: null,
+        boots: null,
+      },
+      archer: {
+        helmet: null,
+        body: null,
+        weapon: null,
+        offHand: null,
+        boots: null,
+      },
+      paladin: {
+        helmet: null,
+        body: null,
+        weapon: null,
+        offHand: null,
+        boots: null,
+      },
+    }
+  );
+
   useEffect(() => {
     localStorage.setItem(
       "gameState",
@@ -32,6 +60,7 @@ export default function GameProvider({ children }) {
         unknownFragments,
         fragmentStacks,
         equipmentsInventory,
+        charactersInventory,
       })
     );
   }, [
@@ -41,6 +70,7 @@ export default function GameProvider({ children }) {
     unknownFragments,
     fragmentStacks,
     equipmentsInventory,
+    charactersInventory,
   ]);
 
   const addUnknownFragment = (fragment) =>
@@ -98,6 +128,46 @@ export default function GameProvider({ children }) {
   const addManaPotion = (amount) => setManaPotions((m) => m + amount);
   const useManaPotion = () => setManaPotions((m) => Math.max(m - 1, 0));
 
+  const equipItem = ({ item, charId }) => {
+    const character = characters.find((c) => c.id === charId);
+
+    if (item.charClass !== character.id) {
+      return;
+    }
+
+    const slot = item.slot;
+
+    const currentItem = charactersInventory[charId][slot];
+
+    setCharactersInventory((prev) => {
+      return { ...prev, [charId]: { ...prev[charId], [slot]: item } };
+    });
+
+    setEquipmentsInventory((prev) => {
+      let updated = prev.filter((i) => i.id !== item.id);
+
+      if (currentItem) {
+        updated = [...updated, currentItem];
+      }
+
+      return updated;
+    });
+  };
+
+  const unequipItem = ({ slot, charId }) => {
+    const currentItem = charactersInventory[charId][slot];
+
+    if (!currentItem) return;
+
+    setCharactersInventory((prev) => {
+      return { ...prev, [charId]: { ...prev[charId], [slot]: null } };
+    });
+
+    setEquipmentsInventory((prev) => {
+      return [...prev, currentItem];
+    });
+  };
+
   return (
     <GameContext.Provider
       value={{
@@ -118,6 +188,9 @@ export default function GameProvider({ children }) {
         removeFragments,
         equipmentsInventory,
         addEquipment,
+        charactersInventory,
+        equipItem,
+        unequipItem,
       }}
     >
       {children}
